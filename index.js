110b1026d7e24bcbb5de993000f97b5276a8794c
cox512 <64932698+cox512@users.noreply.github.com>
2020-08-17 13:55:25 -0500

    heroku deploy 3

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Greetings");
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
